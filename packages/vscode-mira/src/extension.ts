05db2612da8bc60ac18962f872d53be12f9c6d81
slab1 <slab1@users.noreply.github.com>
2026-08-23 02:03:44 +0100

    feat: wire vscode-mira extension into devcontainer and add open web command

--- packages/vscode-mira/src/extension.ts
+++ packages/vscode-mira/src/extension.ts
@@ -1,10 +1,16 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('mira.hello', () => {
+  const hello = vscode.commands.registerCommand('mira.hello', () => {
     vscode.window.showInformationMessage('Hello from Mira!');
   });
-  context.subscriptions.push(disposable);
+
+  const openWeb = vscode.commands.registerCommand('mira.openWeb', () => {
+    const url = 'http://localhost:5173';
+    vscode.env.openExternal(vscode.Uri.parse(url));
+  });
+
+  context.subscriptions.push(hello, openWeb);
 }
 
 export function deactivate() {}
